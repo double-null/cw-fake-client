2397c05d115ebb84277c39637040bdf9f35a7ebb
double-null <double-null@yandex.com>
2025-03-16 18:46:04 +0300

    feat: scenario variants

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,4 +1,26 @@
 <?php
 
+use Doctrine\DBAL\DriverManager;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\ORMSetup;
+
+require_once "vendor/autoload.php";
+
+$config = ORMSetup::createAttributeMetadataConfiguration(
+    paths: [__DIR__ . '/../src/Entities'],
+    isDevMode: true,
+);
+
+$connection = DriverManager::getConnection([
+    'driver' => 'pdo_mysql',
+    'dbname' => 'cw',
+    'user' => 'root',
+    'password' => 'root',
+    'host' => 'mysql',
+    'charset' => 'utf8mb4',
+]);
+
+$em = new EntityManager($connection, $config);
+
 $env = new Symfony\Component\Dotenv\Dotenv;
 $env->load(__DIR__ . "/../.env");
--- tasks/roulette_rolling.php
+++ tasks/roulette_rolling.php
@@ -1,21 +1,27 @@
 <?php
 
-require_once "../vendor/autoload.php";
+date_default_timezone_set('Europe/Moscow');
 
+use CW\Entities\Scenario;
 use CW\Services\Logger;
 
-$client = new \CW\Modules\Client\Services\Client();
-$accounts = \CW\Services\AccountService::load();
+require __DIR__ . '/../bootstrap/app.php';
 
-foreach ($accounts as $account) {
+$client = new \CW\Modules\Client\Services\Client();
+
+$scenario = $em
+    ->getRepository(Scenario::class)
+    ->findOneByTag('roulette_rolling');
+
+foreach ($scenario->getAccounts() as $account) {
     // Авторизируемся
     $authData = $client::makeAction('Auth')
-        ->login($account->login, $account->password);
+        ->login($account->getEmail(), $account->getPassword());
 
     // Три раза запускаем рулетку
     for ($i = 0; $i < 3; $i++) {
         $reward = $client::makeAction('Roulette')->roll();
-        $logMessage = "ACCOUNT: {$account->login} REWARD: {$reward}";
+        $logMessage = "ACCOUNT: {$account->getEmail()} REWARD: {$reward}";
         Logger::log($logMessage, 'cw');
     }
 }
